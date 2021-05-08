0978db56596c7dd152514af21c77be54b508e9a9
Juan Lozoya <juan@functionize.com>
2021-05-07 23:54:53 -0500

    Dependencies update.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatButtonModule, MatCardModule } from '@angular/material';
 import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
